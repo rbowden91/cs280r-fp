34c2eaa6d8fae3d7cfc57df3c0f1b73258b9114b
Rishav Mukherji <rishavmukherji@gmail.com>
2015-04-13 01:31:02 -0400

    some more css

--- errors.js
+++ errors.js
@@ -5,7 +5,15 @@ exports.errors = [{
     "regex" : "(.*?):(\\d*):(\\d*): error: implicit declaration of function '(.*?)' is invalid in C99 \\[-Werror,-Wimplicit-function-declaration\\]\n.*\n.*$",
 
     "callback": function(error_string, filename, line, char, function_name) {
-    	return "<pre class = 'error_message'>" +error_string + "</pre><br>It looks like you're trying to use the function " + function_name + " on line " + line + " of " + filename + ". But clang doesn't seem to know what that function is by line " + line + "! Did you definitely spell the function correctly? If so, then clang needs a prototype for the function. You can do this by including the appropriate header at the top of this file. Alternatively, you can manually write a prototype yourself, or if the function is defined in this file, you can just move the function definition of " + function_name + " above that of the function in which you are trying to use it. Check out: <br><iframe width='960' height='540' src='https://www.youtube.com/embed/bQnyxpf0vk0?list=PLhQjrBD2T380JCGC3qD3nGpqt8iIjx2fV' frameborder='0' allowfullscreen></iframe>";
+    	return "<pre class = 'error_message'>" +error_string + "</pre><br>" +  
+    	"<div class = 'help_message'> It looks like you're trying to use the function " + function_name + 
+    	" on line " + line + " of " + filename + ". But clang doesn't seem to know what that function is" + 
+    	" by line " + line + "! Did you definitely spell the function correctly? If so, then clang needs" + 
+    	"a prototype for the function. You can do this by including the appropriate header at the top of" + 
+    	" this file. Alternatively, you can manually write a prototype yourself, or if the function is " + 
+    	"defined in this file, you can just move the function definition of " + function_name + " above " + 
+    	"that of the function in which you are trying to use it. </div> <div class = 'help_video'>" + 
+    	" Check out: <br><iframe width='960' height='540' src='https://www.youtube.com/embed/bQnyxpf0vk0?list=PLhQjrBD2T380JCGC3qD3nGpqt8iIjx2fV' frameborder='0' allowfullscreen></iframe> </div>";
     }
 },
 {
