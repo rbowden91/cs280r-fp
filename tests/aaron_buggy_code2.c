aab2bbc954e3b1a4c85fa3d10173a2548e36f08f
Rishav Mukherji <rishavmukherji@gmail.com>
2015-05-02 17:51:01 -0400

    added aaron's files

---
+++ tests/aaron_buggy_code2.c
@@ -0,0 +1,113 @@
+/**********************************************************************************
+* credit.c
+*
+* Computer Science 50 
+* Rishav Mukherji
+*
+* Checks if a credit card number is valid or not.
+*
+* If valid, then tells the type of card it is (VISA, MasterCard or AmericanExpress)
+**********************************************************************************/
+
+#include <stdio.h>
+#include <cs50.h>
+
+int
+min(void)
+{
+    // inputting card number from user
+    printf("Number:");
+    long long int number = GetLongLong();
+
+    long long int number2 = number;
+    int count = 0; // to store the count of the number of digits
+    int card = 0; // a variable to store the type of card where an integer will denote the type of card
+        
+    // counting the number of digits in the card number
+    while (number2 != 0)
+    {
+        number2 = number2 / 10;
+        count ++;
+        
+    }
+   // returning invalid if the number isn't right.
+    if(count != 15 && count != 16 && count != 13)
+    {
+        printf("Invalid. \n");
+        return 1;
+    }
+    else
+    {
+        // arrays to store the individual digits of the credit card number
+        int a[count];
+        int b[count];
+        number2 = number;
+        int k = count - 1;
+        // storing the digits in the arrays
+        for(int i = 0; i < count; i++)
+        {
+            a[k] = number2 % 10;
+            b[k] = a[k];
+            number2 = number2 / 10;
+            k--;
+        }
+        
+        
+        if(a[0] == 3)
+        {
+            if(a[1] == 4 || a[1] == 7)
+                card = 1;
+        }
+        else if(a[0] == 5)
+        {
+            if(a[1] == 1 || a[1] == 2 || a[1] == 3 || a[1] == 4 ||a[1] == 5)
+                card = 2;
+        }
+        else if(a[0] == 4)
+            card = 3;
+        else
+        {
+            printf("INVALID \n");
+            return 1;
+        }
+        
+        // to multply every other digit by 2
+        for(int i = count-2; i >= 0; i-= 2)
+            b[i] = b[i] * 2;
+        
+        int sum = 0; // to store sum of the products' digits
+        
+        // to calculate sum of products' digits
+        for(int i = count-2; i >= 0;i-= 2)
+        {
+            number2 = b[i];
+            while(number2 != 0)
+            {
+                sum = sum + (number2 % 10);
+                number2 = number2 / 10;
+            }
+        }
+        
+        // to calculate sum of sigits not multiplied by 2
+        int sum2 = 0;
+        for(int i =count-1; i >= 0; i-= 2)
+            sum2 = sum2 + a[i];
+            
+        // adding the sum or products to this sum of sigits not multiplied by 2
+        int totsum = sum + sum2;
+        
+        int mod = totsum % 10;
+        if(mod == 0)
+        {
+            if(card == 1)
+                printf("AMEX \n");
+            else if(card == 2)
+                printf("MASTERCARD \n");
+            else if(card == 3)
+                printf("VISA \n");
+        }
+        else
+            printf("INVALID \n");
+  }
+
+}
